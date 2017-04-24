27cd6a0d0692fef9c97c57e8a798e110b7620b63
fuyi1984 <332347705@qq.com>
2017-04-24 20:02:09 +0800

    2017-4-21

---
+++ Alfawork/源代码/alfa-web-manager/alfa-service/src/main/java/com/alfa/web/pojo/Role.java
@@ -0,0 +1,60 @@
+package com.alfa.web.pojo;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * Created by Administrator on 2017/4/24.
+ */
+public class Role extends Entity implements Serializable {
+
+    private String role_name;
+
+    private int types;
+
+    private int status;
+
+    private Date create_date;
+
+    private Long bycreater_id;
+
+    public String getRole_name() {
+        return role_name;
+    }
+
+    public void setRole_name(String role_name) {
+        this.role_name = role_name;
+    }
+
+    public int getTypes() {
+        return types;
+    }
+
+    public void setTypes(int types) {
+        this.types = types;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public Date getCreate_date() {
+        return create_date;
+    }
+
+    public void setCreate_date(Date create_date) {
+        this.create_date = create_date;
+    }
+
+    public Long getBycreater_id() {
+        return bycreater_id;
+    }
+
+    public void setBycreater_id(Long bycreater_id) {
+        this.bycreater_id = bycreater_id;
+    }
+}
