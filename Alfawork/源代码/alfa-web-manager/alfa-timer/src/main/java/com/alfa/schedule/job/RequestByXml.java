e1447431b84fd181bc49d0246c149da4be0a5ec8
fuyi1984 <332347705@qq.com>
2017-05-05 11:12:55 +0800

    2017-5-5

---
+++ Alfawork/源代码/alfa-web-manager/alfa-timer/src/main/java/com/alfa/schedule/job/RequestByXml.java
@@ -0,0 +1,11 @@
+package com.alfa.schedule.job;
+
+/**
+ * Created by Administrator on 2017/5/5.
+ */
+public class RequestByXml {
+
+    public void runJob(){
+        System.out.println("alfa-timer RequestByXml!");
+    }
+}
---
+++ Alfawork/源代码/alfa-web-manager/alfa-timer/src/main/java/com/alfa/schedule/main/Main.java
@@ -0,0 +1,16 @@
+package com.alfa.schedule.main;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Created by Administrator on 2017/5/5.
+ */
+public class Main {
+
+
+    public static void main(String[] args){
+
+    }
+}
