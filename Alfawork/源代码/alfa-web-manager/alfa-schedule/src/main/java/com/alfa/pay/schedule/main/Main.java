17eb42273484d5639759724060a40333a483533a
fuyi1984 <332347705@qq.com>
2017-05-05 13:50:59 +0800

    2017-5-5

---
+++ Alfawork/源代码/alfa-web-manager/alfa-schedule/src/main/java/com/alfa/pay/schedule/main/Main.java
@@ -0,0 +1,26 @@
+package com.alfa.pay.schedule.main;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Created by Administrator on 2017/5/5.
+ */
+public class Main {
+
+    private static Logger logger = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) throws InterruptedException {
+        new ClassPathXmlApplicationContext(
+                new String[]{"classpath:application.xml",
+                        "classpath:/config/spring/spring-common.xml",
+                        "classpath:spring-schedule.xml"});
+        // heartbeat
+        while (true) {
+            logger.debug("alfa-pay-schedule is alive.");
+            Thread.sleep(60000);
+        }
+    }
+}
