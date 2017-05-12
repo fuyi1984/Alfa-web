f3b1fca04b329db804302520e7396ca83d5d3030
fuyi1984 <332347705@qq.com>
2017-05-12 16:48:32 +0800

    2017-5-12

--- Alfawork/源代码/alfa-web-manager/alfa-service/src/main/java/com/alfa/web/service/Impl/SysRelevanceServiceImpl.java
+++ Alfawork/源代码/alfa-web-manager/alfa-service/src/main/java/com/alfa/web/service/Impl/SysRelevanceServiceImpl.java
@@ -1,9 +1,20 @@
 package com.alfa.web.service.Impl;
 
+import com.alfa.web.dao.SysRelevanceMapper;
 import com.alfa.web.service.SysRelevanceService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 /**
  * Created by Administrator on 2017/5/12.
  */
+@Service
 public class SysRelevanceServiceImpl implements SysRelevanceService {
+
+    @Autowired
+    private SysRelevanceMapper sysRelevanceMapper;
+
+    private static final Logger logger = LoggerFactory.getLogger(SysRelevanceServiceImpl.class);
 }
