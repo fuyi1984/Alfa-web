9018ae72466436da45858b007ad2b5f96dcd33ea
fuyi1984 <332347705@qq.com>
2017-05-15 15:16:18 +0800

    2017-5-15

--- Alfawork/源代码/alfa-web-manager/alfa-service/src/main/java/com/alfa/web/service/Impl/SysUsersServiceImpl.java
+++ Alfawork/源代码/alfa-web-manager/alfa-service/src/main/java/com/alfa/web/service/Impl/SysUsersServiceImpl.java
@@ -1,6 +1,12 @@
 package com.alfa.web.service.Impl;
 
+import com.alfa.web.dao.SysUsersMapper;
+import com.alfa.web.pojo.SysUsers;
 import com.alfa.web.service.SysUsersService;
+import com.alfa.web.util.pojo.Criteria;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -8,4 +14,24 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class SysUsersServiceImpl implements SysUsersService {
+
+    @Autowired
+    private SysUsersMapper sysUsersMapper;
+
+    private static final Logger logger = LoggerFactory.getLogger(SysUsersServiceImpl.class);
+
+    @Override
+    public int countByParams(Criteria example) {
+        return 0;
+    }
+
+    @Override
+    public int insert(SysUsers record) {
+        return this.sysUsersMapper.insert(record);
+    }
+
+    @Override
+    public int insertSelective(SysUsers record) {
+        return this.sysUsersMapper.insertSelective(record);
+    }
 }
--- Alfawork/源代码/alfa-web-manager/alfa-service/src/main/java/com/alfa/web/service/SysUsersService.java
+++ Alfawork/源代码/alfa-web-manager/alfa-service/src/main/java/com/alfa/web/service/SysUsersService.java
@@ -1,7 +1,34 @@
 package com.alfa.web.service;
 
+import com.alfa.web.pojo.SysUsers;
+import com.alfa.web.util.pojo.Criteria;
+
 /**
  * Created by Administrator on 2017/4/26.
  */
 public interface SysUsersService {
+
+    /**
+     * 查询数据总行数
+     *
+     * @param example 查询条件
+     * @return 行数
+     */
+    int countByParams(Criteria example);
+
+    /**
+     * 插入用户信息
+     *
+     * @param record 用户信息
+     * @return 执行条数
+     */
+    int insert(SysUsers record);
+
+    /**
+     * 选择性插入用户信息
+     *
+     * @param record 用户信息
+     * @return 执行条数
+     */
+    int insertSelective(SysUsers record);
 }
