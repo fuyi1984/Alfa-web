fc627298774ee5fdc204da91242043b529d623c4
fuyi1984 <332347705@qq.com>
2017-07-19 17:00:33 +0800

    2017-7-19

--- Alfawork/源代码/alfa-web-manager/alfa-service/src/main/java/com/alfa/web/service/HostoryOrderStatusService.java
+++ Alfawork/源代码/alfa-web-manager/alfa-service/src/main/java/com/alfa/web/service/HostoryOrderStatusService.java
@@ -1,16 +1,14 @@
-package com.alfa.web.dao;
+package com.alfa.web.service;
 
-import com.alfa.web.pojo.HistoryAddress;
 import com.alfa.web.pojo.HostoryOrderStatus;
 import com.alfa.web.util.pojo.Criteria;
 
 import java.util.List;
 
 /**
- * Created by Administrator on 2017/7/18.
+ * Created by Administrator on 2017/7/19.
  */
-public interface HostoryOrderStatusMapper {
-
+public interface HostoryOrderStatusService {
     /**
      * 根据条件查询记录总数
      */
