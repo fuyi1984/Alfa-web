6763efd9c711992df732ef46619973ef07fba4d7
fuyi1984 <332347705@qq.com>
2017-08-25 17:22:31 +0800

    2017-8-25

--- Alfawork/源代码/alfa-web-manager/alfa-platform-easyui/src/main/webapp/js/sysuser/Init.js
+++ Alfawork/源代码/alfa-web-manager/alfa-platform-easyui/src/main/webapp/js/sysuser/Init.js
@@ -5,10 +5,10 @@
 
 $(function () {
 
-    gtoken = ReadCookie("token");
-
-    if (gtoken != "") {
-        setCurrentUser();
+    // gtoken = ReadCookie("token");
+    //
+    // if (gtoken != "") {
+    //     setCurrentUser();
 
         $('#useradd').window('close');
         $('#userupdate').window('close');
@@ -17,9 +17,9 @@ $(function () {
         initdatagrid();
 
         initcombobox();
-    } else {
-       window.location.href=platform_url+"/pages/home/login.html";
-    }
+    // } else {
+    //    window.location.href=platform_url+"/pages/home/login.html";
+    // }
 
 
 });
@@ -40,17 +40,19 @@ function initdatagrid() {
         fitColumns: true,
         fit: true,
 
-        toolbar: ['-', {
+        //region
+
+        /*toolbar: ['-', {
             id: 'btnSave',
             text: '添加',
             iconCls: 'icon-add',
             handler: function () {
-                /**
+                /!**
                  window.location.href = "/";
 
                  $('#grid').edatagrid('addRow');
 
-                 */
+                 *!/
 
                 //this.href = 'alfa-platform-easyui/pages/sysconfig/edit.html';
 
@@ -124,7 +126,7 @@ function initdatagrid() {
                 console.log(rows);
                 console.log(rows[0].userId);
 
-                /*if (rows.length > 1) {
+                /!*if (rows.length > 1) {
                  $.messager.alert('提示', '请选择一条数据');
                  $('#usergrid').datagrid("clearSelections");
                  return;
@@ -134,7 +136,7 @@ function initdatagrid() {
                  $.messager.alert('提示', '不能删除当前登录用户!');
                  $('#usergrid').datagrid("clearSelections");
                  return;
-                 }*/
+                 }*!/
 
                 //var parm = {"userId": rows[0].userId};
 
@@ -264,6 +266,10 @@ function initdatagrid() {
                 $('#usersearch').window('open');
             }
         }, '-'],
+*/
+        //endregion
+
+        toolbar:"#tb",
 
         idField: 'userId',
 
@@ -366,6 +372,204 @@ function initcombobox() {
             }]
         }
     )
+}
+
+/**
+ * 添加
+ */
+function doAdd(){
+    $('#rolelist').combobox({
+            reload: ws_url + '/rest/roles/findAllRole?token=' + gtoken
+        }
+    )
+
+    $('#useradd').window('open');
+}
+
+/**
+ * 修改
+ */
+function doUpdate(){
+    //var row = $('#usergrid').datagrid('getSelected');
+    var rows = $('#usergrid').datagrid('getSelections');
+
+    //console.log(rows);
+    //console.log(rows[0].userId);
+
+    if (!rows || rows.length == 0) {
+        $.messager.alert('提示', '请选择要修改的数据');
+        return;
+    }
+    else {
+
+        if (rows.length > 1) {
+            $.messager.alert('提示', '请选择一条数据');
+            $('#usergrid').datagrid("clearSelections");
+            return;
+        } else {
+            //window.location.href = "/UserInfo/View/" + row.ID;
+
+            $('#rolelist_update').combobox({
+                reload: ws_url + '/rest/roles/findAllRole?token=' + gtoken
+            })
+
+            $('#form2').form('load', {
+                userId_update: rows[0].userId,
+                realname_update: rows[0].realname,
+                rolelist_update: rows[0].roleId,
+                phone_update: rows[0].username,
+                address_update: rows[0].address,
+                orgname_update: rows[0].orgname
+            });
+
+            $('#userupdate').window('open');
+        }
+
+    }
+
+    //$('#update').window('open');
+}
+
+/**
+ * 删除
+ */
+function doDelete(){
+    var rows = $('#usergrid').datagrid('getSelections');
+
+    if (!rows || rows.length == 0) {
+        $.messager.alert('提示', '请选择要删除的数据');
+        return;
+    }
+
+    console.log(rows);
+    console.log(rows[0].userId);
+
+    /*if (rows.length > 1) {
+     $.messager.alert('提示', '请选择一条数据');
+     $('#usergrid').datagrid("clearSelections");
+     return;
+     }
+
+     if (rows[0].userId == guserid) {
+     $.messager.alert('提示', '不能删除当前登录用户!');
+     $('#usergrid').datagrid("clearSelections");
+     return;
+     }*/
+
+    //var parm = {"userId": rows[0].userId};
+
+    for (var i = 0; i < rows.length; i++) {
+        if (rows[i].userId == guserid) {
+            $.messager.alert('提示', '不能删除当前登录用户!');
+            $('#usergrid').datagrid("clearSelections");
+            return;
+        }
+    }
+
+    var assetList = new Array();
+
+    $.each(rows, function (i, n) {
+        assetList.push(n.userId);
+    });
+
+
+    $.messager.confirm('提示', '是否删除这些数据?', function (r) {
+        if (!r) {
+            return;
+        }
+
+        $.ajax({
+            cache: false,
+            datatype: 'json',
+            contentType: 'application/json;charset=UTF-8',
+            type: "POST",
+            url: ws_url + '/rest/user/batchdeleteUser?token=' + gtoken,
+            data: JSON.stringify(assetList),
+            success: function (msg) {
+                if (msg.status == 'success') {
+                    $.messager.alert('提示', '删除成功！', "info", function () {
+                        $('#usergrid').datagrid("clearSelections");
+                        $('#usergrid').datagrid("reload");
+                    });
+                } else {
+                    $.messager.alert('错误', '删除失败！', "error", function () {
+                        $('#usergrid').datagrid("clearSelections");
+                        $('#usergrid').datagrid("reload");
+                    });
+                }
+            },
+            error: function (xhr) {
+                console.log(xhr);
+                $('#usergrid').datagrid("clearSelections");
+                $.messager.alert('错误', '删除失败！', "error");
+            }
+        });
+    });
+}
+
+/**
+ * 审核
+ */
+function doIsCheck(){
+    var rows = $('#usergrid').datagrid('getSelections');
+
+    if (!rows || rows.length == 0) {
+        $.messager.alert('提示', '请选择需要审核的用户');
+        return;
+    }
+
+    console.log(rows);
+    console.log(rows[0].userId);
+
+    for (var i = 0; i < rows.length; i++) {
+
+        if (rows[i].status == '1') {
+            $.messager.alert('提示', '当前用户已审核,不需要二次审核!');
+            $('#usergrid').datagrid("clearSelections");
+            return;
+        }
+
+    }
+
+    var assetList = new Array();
+
+    $.each(rows, function (i, n) {
+        assetList.push(n.userId);
+    });
+
+
+    $.messager.confirm('提示', '是否需要审核这些用户?', function (r) {
+        if (!r) {
+            return;
+        }
+
+        $.ajax({
+            cache: false,
+            datatype: 'json',
+            contentType: 'application/json;charset=UTF-8',
+            type: "POST",
+            url: ws_url + '/rest/user/batchUpdateUserStatus?token=' + gtoken,
+            data: JSON.stringify(assetList),
+            success: function (msg) {
+                if (msg.status == 'success') {
+                    $.messager.alert('提示', '审核成功！', "info", function () {
+                        $('#usergrid').datagrid("clearSelections");
+                        $('#usergrid').datagrid("reload");
+                    });
+                } else {
+                    $.messager.alert('错误', '审核失败！', "error", function () {
+                        $('#usergrid').datagrid("clearSelections");
+                        $('#usergrid').datagrid("reload");
+                    });
+                }
+            },
+            error: function (xhr) {
+                console.log(xhr);
+                $('#usergrid').datagrid("clearSelections");
+                $.messager.alert('错误', '审核失败！', "error");
+            }
+        });
+    });
 }
 
 
