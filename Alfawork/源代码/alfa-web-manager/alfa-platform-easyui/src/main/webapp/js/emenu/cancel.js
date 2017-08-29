094b406b9ef9ef46c047c10465d68a8dea72f5ca
fuyi1984 <332347705@qq.com>
2017-08-29 12:05:54 +0800

    2017-8-29

--- Alfawork/源代码/alfa-web-manager/alfa-platform-easyui/src/main/webapp/js/emenu/cancel.js
+++ Alfawork/源代码/alfa-web-manager/alfa-platform-easyui/src/main/webapp/js/emenu/cancel.js
@@ -2,5 +2,7 @@
  * Created by Administrator on 2017/8/10.
  */
 function menucancel() {
-    
+    $('#menuadd').window('close');
+    $('#menuedit').window('close');
+    $('#menugrid').datagrid("clearSelections");
 }
