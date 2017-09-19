20f3cd67712f1389d3703359507f58976ba724a1
fuyi1984 <332347705@qq.com>
2017-09-19 14:40:24 +0800

    2017-9-19

--- Alfawork/源代码/alfa-web-manager/alfa-service/src/main/java/com/alfa/web/dao/money/moneyactivitiesconcernMapper.java
+++ Alfawork/源代码/alfa-web-manager/alfa-service/src/main/java/com/alfa/web/dao/money/moneyactivitiesconcernMapper.java
@@ -1,7 +1,38 @@
 package com.alfa.web.dao.money;
 
+import com.alfa.web.pojo.moneyactivities;
+import com.alfa.web.pojo.moneyactivitiesconcern;
+import com.alfa.web.util.pojo.Criteria;
+
+import java.util.List;
+
 /**
  * Created by Administrator on 2017/9/18.
  */
 public interface moneyactivitiesconcernMapper {
+
+    /**
+     * 保存属性不为空的记录
+     */
+    int insertSelective(moneyactivitiesconcern record);
+
+    /**
+     * 根据条件查询记录总数
+     */
+    int countByParams(Criteria example);
+
+    /**
+     * 根据主键批量删除记录
+     */
+    int batchdeleteByPrimaryKey(List<String> list);
+
+    /**
+     * 根据主键更新属性不为空的记录
+     */
+    int updateByPrimaryKeySelective(moneyactivitiesconcern record);
+
+    /**
+     * 根据条件查询记录集
+     */
+    List<moneyactivitiesconcern> selectByParams(Criteria example);
 }
--- Alfawork/源代码/alfa-web-manager/alfa-service/src/main/java/com/alfa/web/service/money/moneyactivitiesServcie.java
+++ Alfawork/源代码/alfa-web-manager/alfa-service/src/main/java/com/alfa/web/service/money/moneyactivitiesServcie.java
@@ -1,7 +1,36 @@
 package com.alfa.web.service.money;
 
+import com.alfa.web.pojo.moneyactivities;
+import com.alfa.web.util.pojo.Criteria;
+
+import java.util.List;
+
 /**
  * Created by Administrator on 2017/9/18.
  */
 public interface moneyactivitiesServcie {
+    /**
+     * 保存属性不为空的记录
+     */
+    int insertSelective(moneyactivities record);
+
+    /**
+     * 根据条件查询记录总数
+     */
+    int countByParams(Criteria example);
+
+    /**
+     * 根据主键批量删除记录
+     */
+    int batchdeleteByPrimaryKey(List<String> list);
+
+    /**
+     * 根据主键更新属性不为空的记录
+     */
+    int updateByPrimaryKeySelective(moneyactivities record);
+
+    /**
+     * 根据条件查询记录集
+     */
+    List<moneyactivities> selectByParams(Criteria example);
 }
