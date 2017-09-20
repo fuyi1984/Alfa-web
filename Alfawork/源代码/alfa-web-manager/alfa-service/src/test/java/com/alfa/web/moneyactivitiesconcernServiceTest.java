4772accbf6b67a7b9b54ca402902d9035be808c0
fuyi1984 <332347705@qq.com>
2017-09-20 14:09:43 +0800

    2017-9-20

--- Alfawork/源代码/alfa-web-manager/alfa-service/src/test/java/com/alfa/web/moneyactivitiesconcernServiceTest.java
+++ Alfawork/源代码/alfa-web-manager/alfa-service/src/test/java/com/alfa/web/moneyactivitiesconcernServiceTest.java
@@ -1,10 +1,16 @@
 package com.alfa.web;
 
+import com.alfa.web.pojo.moneyactivities;
 import com.alfa.web.pojo.moneyactivitiesconcern;
 import com.alfa.web.service.money.moneyactivitiesconcernServcie;
+import com.alfa.web.util.JsonUtil;
+import com.alfa.web.util.pojo.Criteria;
 import org.apache.log4j.Logger;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Administrator on 2017/9/20.
@@ -22,5 +28,28 @@ public class moneyactivitiesconcernServiceTest extends TestBase {
         money.setOpenid("1");
         money.setActivitiesid("2");
         this.moneyactivitiesconcernServcie.insertSelective(money);
+    }
+
+    @Test
+    public void search(){
+        Criteria criteria = new Criteria();
+        List<moneyactivitiesconcern> list=this.moneyactivitiesconcernServcie.selectByParams(criteria);
+        System.out.println(JsonUtil.toJson(list));
+    }
+
+    @Test
+    public void delete(){
+       List<String> list=new ArrayList<String>();
+       list.add("1");
+       this.moneyactivitiesconcernServcie.batchdeleteByPrimaryKey(list);
+    }
+
+    @Test
+    public void update(){
+        moneyactivitiesconcern money=new moneyactivitiesconcern();
+        money.setId(2l);
+        money.setOpenid("2");
+        money.setActivitiesid("2");
+        this.moneyactivitiesconcernServcie.updateByPrimaryKeySelective(money);
     }
 }
